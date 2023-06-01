243b893a652171de04567b2fe3d18e096c8312fd
SwastikaPandey11 <swastika.pandey2020@vitstudent.ac.in>
2023-06-01 14:24:02 +0530

    first commit

--- components/Header.js
+++ components/Header.js
@@ -9,11 +9,11 @@ export default function Header({ second, setSecond }) {
   const [open, setOpen] = useState(false);
   return (
     <header>
-      <nav className="flex filter border-b-2 justify-between bg-white px-4 py-4 h-16 overflow-hidden  items-center">
+      <nav className="flex filter border-b-2 justify-between bg-slate-800 h-16 overflow-hidden  items-center">
         <Link className="flex items-center" href="/">
           <Image
             alt="Logo"
-            src={"/icon.png"}
+            src={"/icon1.png"}
             className="object-contain"
             width={50}
             height={40}
@@ -30,9 +30,7 @@ export default function Header({ second, setSecond }) {
         >
           <FaBars className="h-4  w-4 2x mx-auto items-center" />
         </button>
-        <input
-          type="search"
-          className=" m-4  block w-120 rounded-full border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+        <input type="search" className=" m-4  block w-200 rounded-full border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
           placeholder="Search"
           aria-label="Search"
           aria-describedby="button-addon1"
@@ -61,7 +59,6 @@ export default function Header({ second, setSecond }) {
           </div>
           <FaAngleDown className="my-auto items-center" />
         </button>
-        {/* </div> */}
       </nav>
     </header>
   );
